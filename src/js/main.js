6cd4af010f092986f97ef494ac09b6cfefe304d4
Zaur <zazamuradov@gmail.com>
2020-08-31 03:38:10 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+  $('.slider-items').slick({
+  
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+
+      prevArrow:"<button type='button' class='slider-btn  slider-btn--prev'></button>",
+      prevArrow:"<button type='button' class='slider-btn  slider-btn--next'></button>"
+
+      
+    
+  });
+
+      // планое перемещение 
+      $("#nav").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+  
+});
+
+
+
