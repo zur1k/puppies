f276b18367e8ebef0086ae2f37e8fb7a1ced2af5
Zaur <zazamuradov@gmail.com>
2020-09-01 20:16:07 +0300

    responsive

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,44 @@
-$(document).ready(function(){
+$(document).ready(function () {
   $('.slider-items').slick({
-  
+
     dots: false,
     infinite: true,
     speed: 300,
     slidesToShow: 3,
     slidesToScroll: 1,
-    responsive: [
+    responsive: [{
+        breakpoint: 1300,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: false,
+          arrows: false
+        }
+      },
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 3,
           infinite: true,
-          dots: false
+          dots: false,
+          arrows: false
         }
       },
       {
-        breakpoint: 800,
+        breakpoint: 880,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
-          arrows:false
+          arrows: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
         }
       },
       {
@@ -36,45 +53,44 @@ $(document).ready(function(){
       // instead of a settings object
     ],
 
-      // prevArrow:"<button type='button' class='slider-btn  slider-btn--prev'></button>",
-      prevArrow:"<button class='slider-btn slider-btn--prev' type='button'></button>",
-      nextArrow:"<button class='slider-btn slider-btn--next' type='button'></button>"
-  
+    // prevArrow:"<button type='button' class='slider-btn  slider-btn--prev'></button>",
+    prevArrow: "<button class='slider-btn slider-btn--prev' type='button'></button>",
+    nextArrow: "<button class='slider-btn slider-btn--next' type='button'></button>"
 
-      
-    
+
+
+
   });
 
-      // планое перемещение 
-      $("#nav").on("click","a", function (event) {
-        event.preventDefault();
-        var id  = $(this).attr('href'),
-            top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 1000);
-    });
+  // планое перемещение 
+  $("#nav").on("click", "a", function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({
+      scrollTop: top
+    }, 1000);
+  });
 
 
-    
-    // menu
+
+  // menu
 
 
-    $("#nav-toggle").on("click", function(event) {
-      event.preventDefault();
+  $("#nav-toggle").on("click", function (event) {
+    event.preventDefault();
 
-      $(this).toggleClass("active");
-      $("#nav").toggleClass("active");
+    $(this).toggleClass("active");
+    $("#nav").toggleClass("active");
 
-  }); 
-  $(".nav-menu__link").on("click", function(event) {
-      event.preventDefault();
+  });
+  $(".nav-menu__link").on("click", function (event) {
+    event.preventDefault();
 
-      $("#nav-toggle").toggleClass("active");
-      $("#nav").toggleClass("active");
+    $("#nav-toggle").toggleClass("active");
+    $("#nav").toggleClass("active");
 
-  }); 
+  });
 
-  
+
 });
-
-
-
